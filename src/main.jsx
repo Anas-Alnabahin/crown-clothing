9e1054ad38d0560641c303eaf5ca034978fd42af
anas-alnabahin <anasnb2002nb@gmail.com>
2024-11-07 12:38:38 +0200

    creating product-card component and creating shop page

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
-import "./index.scss";
 import { UserProvider } from "./contexts/user.context.jsx";
+import { ProductsProvider } from "./contexts/products.context.jsx";
+
+import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>

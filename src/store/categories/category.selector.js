4aceb392844aa86689a8c80ea59a16dd233abb17
anas-alnabahin <anasnb2002nb@gmail.com>
2024-12-04 14:01:45 +0200

    creating memoized selector with reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categoriesArray.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+const selectCategoriesArray = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categoriesArray
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategoriesArray],
+  (categoriesArray) =>
+    categoriesArray.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);

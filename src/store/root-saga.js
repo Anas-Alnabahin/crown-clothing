8d1689d27bc3a0fd124da8edfedeb291b86bd8c1
anas-alnabahin <anasnb2002nb@gmail.com>
2024-12-10 14:10:50 +0200

    using saga with user, checking user session

--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,7 +1,8 @@
 import { all, call } from "redux-saga/effects";
 
 import { categoriesSage } from "./categories/category.saga";
+import { userSaga } from "./user/user.saga";
 
 export function* rootSage() {
-  yield all([call(categoriesSage)]);
+  yield all([call(categoriesSage), call(userSaga)]);
 }

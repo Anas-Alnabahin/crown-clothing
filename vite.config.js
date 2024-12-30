ceec2aa2a2a5ed72c916dd63b50cc09103700670
anas-alnabahin <anasnb2002nb@gmail.com>
2024-12-30 11:48:30 +0200

    integrating Babel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),svgr()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: ["macros"],
+      },
+    }),
+    svgr(),
+  ],
+});

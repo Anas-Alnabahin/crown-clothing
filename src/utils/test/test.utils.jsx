bd336881b1fccf769dfdeb326bfffd4b226dc53a
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-15 15:39:59 +0300

    creating render with providers utility function and testing cart icon component

---
+++ src/utils/test/test.utils.jsx
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+import { rootReducer } from "../../store/root-reducer";
+
+export function renderWithProviders(
+  ui,
+  {
+    preloadedState = {},
+    store = createStore(rootReducer, preloadedState),
+    ...renderOptions
+  } = {}
+) {
+  const Wrapper = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
